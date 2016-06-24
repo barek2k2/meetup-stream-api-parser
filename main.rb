25dc9fef42f624f58a8772554a0c5b273d28a9c8
Abdul Barek <barek2k2@gmail.com>
2016-06-24 21:48:45 +0600

    stream api query parameters(like since_mtime) supported

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require './stream_client.rb'
+require 'date'
 
 redis_options = {:host => 'localhost',:port => 6379, :event_list => 'events'}
+stream_query_options = {}
 
 # Infinite loop to ensure reconnection automatically in case of any interruption
 loop do
   begin
-    stream_client = StreamClient.new('stream.meetup.com','/2/open_events',80,redis_options)
+    stream_client = StreamClient.new('stream.meetup.com','/2/open_events',80,redis_options,stream_query_options)
     stream_client.listen_and_save
   rescue Exception => e
     puts "Interrupted/Error because of :: #{e.message}"
--- stream_client.rb
+++ stream_client.rb
@@ -1,6 +1,7 @@
 require 'socket'
 require 'redis'
 require 'json'
+require 'uri'
 
 # Stream Client class for Stream API
 class StreamClient
@@ -10,14 +11,18 @@ class StreamClient
   # * +port(Integer)+ - Port of the stream API, default is 80
   #
   # ==== redis_options
-  #
   # Hash of Redis server
-  #
   # * +:host(String)+ - Redis Server host like "localhost or an IP"
   # * +:port(Integer)+ - Redis server port, default is 6379
-  def initialize(host,path,port=80, redis_options={})
+  #
+  # ==== stream_query_options
+  # Hash of query parameter
+  # * +:since_mtime(Integer)+ - Time in milliseconds like "(Date.today+10).strftime('%Q')"
+  # More example of query parameters at http://www.meetup.com/meetup_api/docs/stream/2/open_events/
+  def initialize(host,path,port=80, redis_options={},stream_query_options={})
     @host = host
     @path = path
+    @path = @path + "?" + URI.encode(stream_query_options.map{|k,v| "#{k}=#{v}"}.join("&")) if stream_query_options.keys.any?
     @port = port    
     @redis ||= Redis.new(:host => redis_options[:host] || "localhost", :port => redis_options[:port] || 6379)
     @event_list = redis_options[:event_list]
